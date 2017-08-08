6bb5270ca1ead2c69fa82231e36446e53b287c35
Jesse Nieminen <jesse.nieminen@intel.com>
2017-08-08 13:07:05 +0300

    Modify sensor class

--- scripts/app.js
+++ scripts/app.js
@@ -31,10 +31,9 @@ class InclinationSensor {
         sensor_.onerror = () => {
                 if (this.onerror) this.onerror();
         }
-        //Object.assign(this, { start });
         }
         start() {
-                sensor.start();
+                Object.assign(this, { start });
         }
         get roll() {
                 return this.euler[0];
