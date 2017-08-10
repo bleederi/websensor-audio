ecae1051ae08bcb8a390aabd7bf938bb0224e22e
Jesse Nieminen <jesse.nieminen@intel.com>
2017-08-10 11:31:17 +0300

    Landscape mode fix orientation

--- scripts/app.js
+++ scripts/app.js
@@ -169,7 +169,7 @@ function render() {
                 {
                         console.log(sensor.roll, sensor.pitch, sensor.yaw);
                         var longitudeRad = -sensor.pitch;
-                        var latitudeRad = sensor.roll + Math.PI;
+                        var latitudeRad = sensor.roll + Math.PI/2;
                         
                 
                 }
