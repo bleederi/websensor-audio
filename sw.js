60db3323f43561c8cda381b277ef52c49b4f4015
Jesse Nieminen <jesse.nieminen@intel.com>
2017-08-16 13:20:05 +0300

    Resource caching fix

--- sw.js
+++ sw.js
@@ -17,15 +17,26 @@ self.addEventListener('install', function(event) {
 
 this.addEventListener('fetch', function(event) {
 //Retrieval from cache
-  event.respondWith(
-    caches.match(event.request).then(function(resp) {
-      return resp || fetch(event.request).then(function(response) {
-        caches.open(CACHE_VERSION.toString()).then(function(cache) {
-          cache.put(event.request, response.clone());
+  event.respondWith(caches.match(event.request).then(function(response) {
+    // caches.match() always resolves
+    // but in case of success response will have value
+    if (response !== undefined) {
+      return response;
+    } else {
+      return fetch(event.request).then(function (response) {
+        // response may be used only once
+        // we need to save clone to put one copy in cache
+        // and serve second one
+        let responseClone = response.clone();
+        
+        caches.open(CACHE_VERSION.toString()).then(function (cache) {
+          cache.put(event.request, responseClone);
         });
         return response;
+      }).catch(function () {
+        return caches.match('resources/beach_dinner.jpg');
       });
-    })
-  );
+    }
+  }));
 });
 
