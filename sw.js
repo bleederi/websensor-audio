9ab3135aac520c1ac41f8371682849cbb8007b11
Jesse Nieminen <jesse.nieminen@intel.com>
2017-08-16 12:52:46 +0300

    Resource caching fix

--- sw.js
+++ sw.js
@@ -7,9 +7,9 @@ self.addEventListener('install', function(event) {
     caches.open(CACHE_VERSION.toString()).then(function(cache) {
       return cache.addAll([
         'index.html',
-        APP_NAME + '/scripts/app.js',
-        APP_NAME + '/resources/beach_dinner.jpg',
-        APP_NAME + '/resources/ocean.mp3',
+        'scripts/app.js',
+        'resources/beach_dinner.jpg',
+        'resources/ocean.mp3',
       ]);
     })
   );
