fb6d32d20d8a1471d995a4276f5c13c2f18a30f7
Jesse Nieminen <jesse.nieminen@intel.com>
2017-08-16 13:07:53 +0300

    Resource caching fix

--- sw.js
+++ sw.js
@@ -19,7 +19,10 @@ this.addEventListener('fetch', function(event) {
 //Retrieval from cache
   event.respondWith(
     caches.match(event.request).then(function(resp) {
-      return resp || fetch(event.request).then(function(response) {
+      return resp || fetch(event.request, {
+  credentials: 'include',
+  redirect: 'follow'
+}).then(function(response) {
         caches.open(CACHE_VERSION.toString()).then(function(cache) {
           cache.put(event.request, response.clone());
         });
