e98f4d5f3dde84af34dbd185984f10344ff3d36f
Jesse Nieminen <jesse.nieminen@intel.com>
2017-08-16 12:46:18 +0300

    Resource caching fix

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_VERSION = 'v1';
+const CACHE_VERSION = 1;
 
 self.addEventListener('install', function(event) {
 //Caching
   event.waitUntil(
-    caches.open(CACHE_VERSION).then(function(cache) {
+    caches.open(CACHE_VERSION.toString()).then(function(cache) {
       return cache.addAll([
         'index.html',
         '/scripts/app.js',
@@ -19,7 +19,7 @@ this.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request).then(function(resp) {
       return resp || fetch(event.request).then(function(response) {
-        caches.open(CACHE_VERSION).then(function(cache) {
+        caches.open(CACHE_VERSION.toString()).then(function(cache) {
           cache.put(event.request, response.clone());
         });
         return response;
