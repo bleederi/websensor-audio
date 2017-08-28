313ab923462b26a533316f9c422eb33d95cf2ac7
Jesse Nieminen <jesse.nieminen@intel.com>
2017-08-28 15:20:54 +0300

    Service worker

--- scripts/app.js
+++ scripts/app.js
@@ -92,9 +92,7 @@ function init() {
     if ('serviceWorker' in navigator) {
             window.addEventListener('load', function() {
                     navigator.serviceWorker.register('sw.js').then(function(registration) {
-                            // Registration was successful
                     }, function(err) {
-                            // Registration failed
                     console.log('ServiceWorker registration failed: ', err);
                     });
             });
